b22b6cf961531ad56d3e6cf099262d1fd3e4ec7e
sergiomsil <sergiomsil@hotmail.com>
2019-08-09 10:54:24 -0300

    inicial huntweb

---
+++ src/pages/main/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+
+export default class Main extends Component {
+    render() {
+        <h1>Hello Rocketseat</h1>;
+    }
+}
