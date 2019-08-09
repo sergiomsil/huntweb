dbbe2be5ca200329473e24eb6833870a5ceb855e
sergiomsil <sergiomsil@hotmail.com>
2019-08-09 11:44:20 -0300

    acessando api

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
-import api from "../../services/api";
+import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
+    componentDidCatch() {
+        this.loadProducts();
+    }
+
+    loadProducts =async () => {
+        const response = await api.get("/products");
+
+        this.setState({ products: response.data.docs });
+    };
+
     render() {
-        <h1>Hello Rocketseat</h1>;
+       return <h1>Contagem de produtos: {this.state.products.lenght}</h1>;
     }
 }
